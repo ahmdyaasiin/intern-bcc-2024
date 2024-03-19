ba2990a09b973aec52c2fe67e2da39961d884919
ayas <ahmdyaasiin@student.ub.ac.id>
2024-03-20 03:31:32 +0700

    fix midtrans for create and verify payment

--- pkg/midtrans/midtrans.go
+++ pkg/midtrans/midtrans.go
@@ -1,28 +1,56 @@
 package midtrans
 
 import (
+	"github.com/google/uuid"
 	"github.com/midtrans/midtrans-go"
+	"github.com/midtrans/midtrans-go/coreapi"
 	"github.com/midtrans/midtrans-go/snap"
 	"intern-bcc-2024/entity"
 	"intern-bcc-2024/pkg/response"
+	"os"
 )
 
-var s snap.Client
-
-func CreateToken(product *entity.Product) (string, response.Details) {
-	serverKey := "SB-Mid-server-TQt21w4SNEN7CYjLvOt6vQs1"
-	s.New(serverKey, midtrans.Sandbox)
-
-	resp, err := s.CreateTransactionToken(&snap.Request{
+func CreateToken(idTransaction uuid.UUID, product *entity.Product) (string, response.Details) {
+	serverKey := os.Getenv("MIDTRANS_SERVER_KEY")
+	req := &snap.Request{
 		TransactionDetails: midtrans.TransactionDetails{
-			OrderID:  product.ID.String(),
+			OrderID:  idTransaction.String(),
 			GrossAmt: int64(product.Price),
 		},
-		EnabledPayments: snap.AllSnapPaymentType,
-	})
+		EnabledPayments: []snap.SnapPaymentType{
+			snap.PaymentTypeGopay,
+			snap.PaymentTypeShopeepay,
+			snap.PaymentTypeBankTransfer,
+		},
+		Expiry: &snap.ExpiryDetails{
+			Duration: 1,
+			Unit:     "minute",
+		},
+	}
+
+	var client snap.Client
+	client.New(serverKey, midtrans.Sandbox)
+	client.Options.SetPaymentOverrideNotification("https://seahorse-cool-kodiak.ngrok-free.app/api/v1/product/payment/callback")
+
+	snapResp, err := client.CreateTransactionToken(req)
 	if err != nil {
 		return "", response.Details{Code: 500, Message: "Failed to create token", Error: err}
 	}
 
-	return resp, response.Details{Code: 201, Message: "Success to create token", Error: nil}
+	return snapResp, response.Details{Code: 201, Message: "Success to create token", Error: nil}
+}
+
+func VerifyPayment(idTransaction uuid.UUID) (*coreapi.TransactionStatusResponse, error) {
+	serverKey := os.Getenv("MIDTRANS_SERVER_KEY")
+
+	var client coreapi.Client
+	client.New(serverKey, midtrans.Sandbox)
+
+	// 4. Check transaction to Midtrans with param orderId
+	transactionStatusResp, e := client.CheckTransaction(idTransaction.String())
+	if e != nil {
+		return nil, e
+	} else {
+		return transactionStatusResp, nil
+	}
 }
